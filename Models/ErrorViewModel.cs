d8c3d9f4fe76259289561f8405085c25e5490d9e
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-09-20 15:24:48 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestOpenXml.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
